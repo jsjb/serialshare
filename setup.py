f10c7f82b1ffb5cee6e4c2c2ddb05ac56a61ef80
Jadon Bennett <git@jben.ca>
2020-07-29 02:19:07 -0700

    create setup.py, remove requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A setup script for serialshare """
+
+from distutils.core import setup
+
+setup(name='serialshare',
+      version='1.0',
+      packages=['serialshare', 'serialshare-server'],
+      install_requires=['pyserial==3.4', 'websockets==8.1']
+     )
