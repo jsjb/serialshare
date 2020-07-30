eb8e487246f35a2b86f8d905b23c2b37c96e788e
Jadon Bennett <git@jben.ca>
2020-07-29 19:27:12 -0700

    Begin server component
    currently very rudimentary, broken, TODO, etc.

--- serialshare-server/__main__.py
+++ serialshare-server/__main__.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python3
-"""
-Starts an HTTP server and listens for websocket connections
-when one is made, connects it to the console
-"""
+#!/usr/bin/env python
 
+import asyncio
 import websockets
 
+async def get_messages(websocket):
+    print('listening for messages')
+    async for message in websocket:
+        print(bytes.decode(message, encoding='utf-8'))
 
-def main():
-    pass
+async def send_input(websocket):
+    print('getting input')
+    val = ""
+    while val != 'quit':
+        val = input("")
+        await websocket.send(val + "\n")
 
+async def consume(websocket, path):
+    await asyncio.wait([get_messages(websocket), send_input(websocket)])
 
-if __name__ == '__main__':
-    main()
+start_server = websockets.serve(consume, "localhost", 8089)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
