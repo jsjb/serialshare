78b6cd1020e834ad0a824aa4790a0d7de696a686
Jadon Bennett <git@jben.ca>
2020-08-06 14:34:09 -0700

    add aiopipe dependency

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(name='serialshare',
       version='1.0',
       packages=['serialshare', 'serialshare-server'],
       install_requires=[
+          'aiopipe>0.2,<0.3',
           'appdirs>1.4,<1.5',
           'asciimatics==1.11',
           'pyserial==3.4',
           'pyserial-asyncio==0.4',
-          'pyte>=0.8.0,<=0.9'
+          'pyte>=0.8.0,<=0.9',
           'websockets==8.1'
       ]
       )
